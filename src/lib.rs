23405d1e99592baf05d7a3aa02abd6982a62b90b
Pierre Avital <pierre.avital@me.com>
2021-11-09 11:36:27 +0100

    rust calling convention for create_backend

--- src/lib.rs
+++ src/lib.rs
@@ -54,7 +54,7 @@ lazy_static::lazy_static!(
 const CREATE_BACKEND_TYPECHECK: CreateBackend = create_backend;
 
 #[no_mangle]
-pub extern "C" fn create_backend(_unused: BackendConfig) -> ZResult<Box<dyn Backend>> {
+pub fn create_backend(_unused: BackendConfig) -> ZResult<Box<dyn Backend>> {
     // For some reasons env_logger is sometime not active in a loaded library.
     // Try to activate it here, ignoring failures.
     let _ = env_logger::try_init();
