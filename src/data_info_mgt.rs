f287f6ec33520f651ce744c8759360dbfc355111
Julien Enoch <julien.enoch@adlinktech.com>
2022-12-12 12:23:28 +0100

    Rollback to RocksDB 0.18.0

--- src/data_info_mgt.rs
+++ src/data_info_mgt.rs
@@ -158,19 +158,14 @@ impl DataInfoMgr {
     }
 
     pub async fn get_deleted_entries(&self) -> Vec<(String, Timestamp)> {
-        let mut entries = Vec::new();
+        let mut result = Vec::new();
         let db = self.db.lock().await;
-        for result in db.iterator(IteratorMode::Start) {
-            if result.is_err() {
-                log::debug!("Error getting deleted entries: '{}'", result.unwrap_err());
-                continue;
-            }
-            let (key, value) = result.unwrap();
+        for (key, value) in db.iterator(IteratorMode::Start) {
             if let Ok(path) = std::str::from_utf8(&key).map(Path::new) {
                 if !path.exists() {
                     match decode_timestamp_from_value(&value) {
                         Ok(timestamp) => {
-                            entries
+                            result
                                 .push((std::str::from_utf8(&key).unwrap().to_string(), timestamp));
                         }
                         Err(e) => warn!("Failed to decode data-info for file {:?}: {}", path, e),
@@ -178,7 +173,7 @@ impl DataInfoMgr {
                 }
             }
         }
-        entries
+        result
     }
 }
 
@@ -226,12 +221,7 @@ impl Timed for GarbageCollectionEvent {
         let time_limit = NTP64::from(SystemTime::now().duration_since(UNIX_EPOCH).unwrap())
             - *MIN_DELAY_BEFORE_REMOVAL;
         let db = self.db.lock().await;
-        for result in db.iterator(IteratorMode::Start) {
-            if result.is_err() {
-                log::debug!("Error during garbage collection: '{}'", result.unwrap_err());
-                continue;
-            }
-            let (key, value) = result.unwrap();
+        for (key, value) in db.iterator(IteratorMode::Start) {
             if let Ok(path) = std::str::from_utf8(&key).map(Path::new) {
                 if !path.exists() {
                     // check if path was marked as deleted for a long time
